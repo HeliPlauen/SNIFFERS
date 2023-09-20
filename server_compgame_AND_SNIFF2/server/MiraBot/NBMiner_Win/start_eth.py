2a5307672698bbe692f938a0def3d8a1ac843969
HeliPlauen <oleg.shcherbatiouk@gmail.com>
2023-09-20 11:53:18 +0300

    2-nd commit: some M changes

---
+++ server_compgame_AND_SNIFF2/server/MiraBot/NBMiner_Win/start_eth.py
@@ -0,0 +1,10 @@
+import time
+
+
+# start_eth.bat
+while True:
+    print("**********************************")
+    print("This is Miner!!!!")
+    print("This is Miner!!!!")
+    print("A-HA-HA-HA-HA!!!!")
+    time.sleep(1)
